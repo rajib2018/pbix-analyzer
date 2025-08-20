2739ae9898da3f18e275a7075847946fd2e4b6ef
Rajib Bhattacharya <38496316+rajib2018@users.noreply.github.com>
2025-08-20 09:51:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,65 @@
 import streamlit as st
-import tempfile
-import os
-from pbixray import PBIXRay
+from pbixray.core import PBIXRay
 
-st.title("PBIX File Analyzer and Document Generator")
-st.write("Upload your Power BI (.pbix) file to analyze its structure and generate detailed documentation.")
+def sizeof_fmt(num, suffix="B"):
+    for unit in ("", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"):
+        if abs(num) < 1024.0:
+            return f"{num:3.1f}{unit}{suffix}"
+        num /= 1024.0
+    return f"{num:.1f}Yi{suffix}"
 
-uploaded_file = st.file_uploader("Choose a .pbix file", type="pbix")
+def app():
+    st.title("PBIX info")
 
-if uploaded_file is not None:
-    st.write("File uploaded successfully!")
+    uploaded_file = st.file_uploader("Choose a PBIX file", type="pbix")
+    if uploaded_file:
+        # Unpack the PBIX file to get the schema_df
+        model = PBIXRay(uploaded_file)
 
-model = PBIXRay(uploaded_file)
-tables = model.tables
-print(tables)
+
+        st.write(model.metadata)
+        
+        met1, met2 = st.columns(2)
+        
+        met1.metric(label='Model size', value = sizeof_fmt(model.size))
+        met2.metric(label='# Tables', value = model.tables.size)
+
+        st.write("Schema:")
+        st.write(model.schema)
+
+        st.write("Statistics:")
+        st.dataframe(model.statistics)
+
+        if model.relationships.size:
+            st.write("Relationships:")
+            st.write(model.relationships)
+
+        if model.power_query.size:
+            st.write("Power Query code:")
+            st.dataframe(model.power_query)
+
+        if model.m_parameters.size:
+            st.write("M parameters:")
+            st.dataframe(model.m_parameters)
+
+        if model.dax_tables.size:
+            st.write("DAX tables:")
+            st.dataframe(model.dax_tables)
+        
+        if model.dax_measures.size:
+            st.write("DAX measures:")
+            st.dataframe(model.dax_measures)
+
+        if model.dax_columns.size:
+            st.write("Calculated columns:")
+            st.dataframe(model.dax_columns)
+            
+        # Let the user select a table name
+        table_name_input = st.selectbox("Select a table to peek at its contents:", model.tables)
+
+        if st.button("Un-VertiPaq"):
+            st.dataframe(model.get_table(table_name_input), use_container_width=True)
+
+
+if __name__ == '__main__':
+    app()
